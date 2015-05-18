3f1e83f0e45b1a3ad61127f2c421ea6ddba79528
Abraham Kiggundu <akiggund@thoughtworks.com>
2015-05-18 14:41:27 +0300

    emptied all scripts in order to demonstrate build

--- scripts/packaging/package-build.sh
+++ scripts/packaging/package-build.sh
@@ -5,4 +5,3 @@ set -e
 mkdir build
 echo "empty build image" >> build/contacts_docker_image.tar
 cp scripts/packaging/install-image-contacts.sh build/install-image-contacts.sh
-cp -r scripts/deployment build/deployment
